4d021998ad1572c49d87688ff00ef129b238b0b3
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-20 23:11:38 +0200

    Implement progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
+
+const progress = new ProgressBar({
+  // The size (height) of the progress bar.
+  // Numeric values get converted to px.
+  size: 4,
+
+  // Color of the progress bar.
+  // Also used for the glow around the bar.
+  color: '#fe595e',
+
+  // Class name used for the progress bar element.
+  className: 'z-50',
+
+  // How many milliseconds to wait before the progress bar
+  // animation starts after calling .start().
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
