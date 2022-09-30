c90fae54f9aa0beb6d073e4a6661548ba882ffa4
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-30 23:44:33 +0200

    Change map marker styling

--- components/MapContainer.jsx
+++ components/MapContainer.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map, { Marker, Popup } from 'react-map-gl';
 import { getCenter } from 'geolib';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { HomeIcon } from '@heroicons/react/24/solid';
 
 const MapContainer = ({ searchResults }) => {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const coordinates = searchResults?.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
@@ -11,38 +15,55 @@ const MapContainer = ({ searchResults }) => {
   const center = getCenter(coordinates);
 
   return (
-    <>
-      <Map
-        initialViewState={{
-          longitude: center.longitude,
-          latitude: center.latitude,
-          zoom: 8,
-        }}
-        style={{ width: '100%', height: '100%' }}
-        mapStyle="mapbox://styles/ahmed2mohamed/cl8bjaxqj001v14mtj812crm8"
-        mapboxAccessToken={process.env.MAP_BOX_KEY}
-      >
-        {/* {searchResults?.map((result) => (
+    <Map
+      initialViewState={{
+        longitude: center.longitude,
+        latitude: center.latitude,
+        zoom: 8,
+      }}
+      style={{ width: '100%', height: '100%' }}
+      mapStyle="mapbox://styles/ahmed2mohamed/cl8bjaxqj001v14mtj812crm8"
+      mapboxAccessToken={process.env.MAP_BOX_KEY}
+    >
+      {searchResults?.map((result) => (
         <div key={result.long}>
           <Marker
-          longitude={result.long}
+            longitude={result.long}
             latitude={result.lat}
-            offsetLeft={-20}
-            offsetTop={-10}
-            >
-            <p
-            role="img"
-            className="cursor-pointer text-2xl animate-bounce"
-            aria-label="push-pin"
-            >
-              📌
+            offset={[-20, -10]}
+          >
+            {result.price_per_night ? (
+              <p
+                className="cursor-pointer text-sm bg-gray-100 font-semibold py-0.5 px-2 rounded-xl hover:scale-110 transition transform ease-out active:bg-black active:text-white "
+                onClick={() => {
+                  setSelectedLocation(result);
+                }}
+              >
+                ${result.price_per_night}
               </p>
-              </Marker>
+            ) : (
+              <div className="bg-[#fd5b605e] rounded-full w-24 h-24 flex items-center justify-center">
+                <div className="bg-[#fd5b61] w-12 h-12 rounded-full flex items-center justify-center">
+                  <HomeIcon color="white" className="w-8" />
+                </div>
               </div>
-            ))} */}
-      </Map>
-
-    </>
+            )}
+          </Marker>
+          {/* {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              <p className="bg-red-500">A7a</p>
+            </Popup>
+          ) : (
+            false
+          )} */}
+        </div>
+      ))}
+    </Map>
   );
 };
 
