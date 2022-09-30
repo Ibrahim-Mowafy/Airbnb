95b82018ce94da972379b66e1d49ac5aa93a02b2
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-30 23:46:57 +0200

    Adding sanity domain for Next Images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['links.papareact.com'],
+    domains: ['links.papareact.com', 'cdn.sanity.io'],
   },
   i18n: {
     locales: ['en'],
