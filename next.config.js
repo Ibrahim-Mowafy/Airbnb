526e733129a6fd7b6d5e27ef5c877fc3bb6f83b4
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-09-30 23:58:09 +0200

    Adding env for stripe and map box

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+  env: {
+    MAP_BOX_KEY:
+      'pk.eyJ1IjoiYWhtZWQybW9oYW1lZCIsImEiOiJjbDhiaDluYXowcjFqM3BuM3FpdWhhdjM4In0.7H1O6fjUBySNQUXHloPK2A',
+    STRIPE_PUBLISHABLE_KEY:
+      'pk_test_51Lld0rCESh4YsU5s9g9q1dLELUMN9BBhWlobwau5Yady1Hh4nB0hEiLPg3MHeIHKH294Flqt90gqTQ6qn5DM892A005QAsoqVO',
+    STRIPE_SECRET_KEY:
+      'sk_test_51Lld0rCESh4YsU5sT1uyD30pNTDX5iqJJMgavbIfES6LXGJgQUTerW6aGQM2bntL7ymIOywoTH6f84cF9sYImwNC00VOYr0VfV',
+  },
 };
 
 module.exports = nextConfig;
