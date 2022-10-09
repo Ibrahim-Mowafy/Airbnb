0dacb5ad8f4e1a14349f5ced5dc9fea16ecd4422
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-10-09 18:36:46 +0200

    Change icons in categories component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,60 +1,9 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { HiOutlineHome } from 'react-icons/hi';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { Navigation } from 'swiper';
-
-const categories = [
-  {
-    name: 'National parks',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Beach',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Islands',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Windmills',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Tiny homes',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'OMG!',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Amazing pools',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Arctic',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Design',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Shared homes',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Caves',
-    icon: <HiOutlineHome />,
-  },
-  {
-    name: 'Amazing views',
-    icon: <HiOutlineHome />,
-  },
-];
+import Image from 'next/image';
+import { categories } from '../utils/constants';
 
 const Categories = () => {
   const router = useRouter();
@@ -68,14 +17,21 @@ const Categories = () => {
     <SwiperSlide key={category.name}>
       <Link href={`/?category=${category.name}`}>
         <div
-          className={`flex flex-col justify-center items-center cursor-pointer text-gray-500 hover:text-black transition hover:border-b-2 hover:border-gray-300 pb-2 active:scale-95  transform duration-150 ease-out  ${
+          className={`flex flex-col justify-center items-center cursor-pointer filter-custom  hover:text-black transition-all hover:border-b-2 hover:border-gray-300 pb-2 active:scale-95 hover:filter-none  transform duration-150 ease-out  ${
             categorySelected === category.name
-              ? 'text-black border-b-2 border-black pointer-events-none'
+              ? '!filter-none  border-b-2 border-black pointer-events-none'
               : ''
           }`}
         >
-          <span className="text-3xl ">{category.icon}</span>
-          <span className="text-sm pt-0.5 text-center whitespace-nowrap">
+          <span className="text-3xl">
+            <Image
+              src={category.icon}
+              alt={category.name}
+              width={25}
+              height={25}
+            />
+          </span>
+          <span className="text-xs font-semibold pt-0.5 text-center whitespace-nowrap">
             {category.name}
           </span>
         </div>
@@ -87,8 +43,8 @@ const Categories = () => {
     <section className="categories pt-8 sticky top-20 bg-white z-40 shadow-sm  w-full">
       <div className="mx-auto px-8 sm:px-16 ">
         <Swiper
-          slidesPerView={10}
-          spaceBetween={50}
+          slidesPerView={12}
+          spaceBetween={40}
           grabCursor={true}
 
           // modules={[Navigation]}
