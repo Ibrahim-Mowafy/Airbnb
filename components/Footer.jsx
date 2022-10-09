aad73b4e0f7efe801c10961a426920adf97eabff
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-10-09 19:41:48 +0200

    Adding footer content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,48 +1,19 @@
 import React from 'react';
+import { footer } from '../utils/constants';
 
 const Footer = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investor</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investor</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investor</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investor</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investor</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center gap-y-10 px-8 sm:px-16 py-14 bg-gray-100 text-gray-600">
+      {footer.map((item) => (
+        <div key={item.title} className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">{item.title}</h5>
+          {item.content.map((itemContent) => (
+            <p key={itemContent} className="cursor-pointer hover:underline">
+              {itemContent}
+            </p>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
