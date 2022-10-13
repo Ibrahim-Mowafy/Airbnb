2360574c7b57e1001f05a8121bb335bc50a46965
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-10-13 20:18:41 +0200

    Adding wishlist guard content

--- pages/wishlists.js
+++ pages/wishlists.js
@@ -1,21 +1,53 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Head from 'next/head';
 import React, { useContext, useState } from 'react';
 import Header from '../components/Header';
 import MapContainer from '../components/MapContainer';
 import RoomItem from '../components/RoomItem';
 import { WishlistsContext } from '../context/wishlist-context';
+import { ModalContext } from '../context/modal-context';
+import { useSession } from 'next-auth/react';
 
 const wishlists = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const wishlists = useContext(WishlistsContext).wishlist;
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { wishlist } = useContext(WishlistsContext);
   const [isHoverRoomItem, setIsHoverRoomItem] = useState(false);
+  const { data: session } = useSession();
+  const { openModal } = useContext(ModalContext);
 
-  if (wishlists.length === 0) {
+  if (!session) {
     return (
       <>
         <Head>
-          <title>Your list . Wishlist - Airbnb</title>
+          <title>{'Your list . Wishlist - Airbnb'}</title>
+        </Head>
+        <Header />
+        <main className="flex flex-col p-5 md:px-10 relative w-full h-full">
+          <h1 className="text-3xl font-bold mt-2 mb-6">Wishlist</h1>
+
+          <h3 className="text-2xl font-semibold">
+            Log in to view your wishlists
+          </h3>
+          <p className="text-gray-500 mt-5 max-w-lg">
+            {"You can create, view, or edit wishlists once you've logged in."}
+          </p>
+          <div>
+            <button
+              className="button py-2 px-5 rounded-lg mt-5 bg-[#fd5b61] text-white active:bg-[#ff7075] font-semibold text-lg "
+              onClick={openModal}
+            >
+              Log in
+            </button>
+          </div>
+        </main>
+      </>
+    );
+  }
+
+  if (session && wishlist.length === 0) {
+    return (
+      <>
+        <Head>
+          <title>{'Your list . Wishlist - Airbnb'}</title>
         </Head>
         <Header />
         <main className="flex flex-col p-5 md:px-10 relative w-full h-full">
@@ -33,13 +65,16 @@ const wishlists = () => {
 
   return (
     <>
+      <Head>
+        <title>{'Your list . Wishlist - Airbnb'}</title>
+      </Head>
       <Header />
       <main className="flex pb-5  relative w-full h-full">
         <section className="flex-grow pt-14 px-6">
           <h1 className="text-3xl font-semibold mt-2 mb-6">Wishlist</h1>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 ">
-            {wishlists?.map((roomData) => (
+            {wishlist?.map((roomData) => (
               <>
                 <div
                   onMouseEnter={() => {
@@ -58,7 +93,7 @@ const wishlists = () => {
         <section className="flex-grow min-w-[50%] relative ">
           <div className="h-[100vh] sticky top-24">
             <MapContainer
-              searchResults={wishlists}
+              searchResults={wishlist}
               onHoverItems={isHoverRoomItem}
               addHeart={true}
             />
