1404c594218b5c5005a1c584a4fa37de39521794
Ibrahim-Mowafy <ibrahim.mowafy10@gmail.com>
2022-10-15 23:15:25 +0200

    Adding accent color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#ff385c',
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
