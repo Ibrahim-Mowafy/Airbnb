df548f1e8f3496aa80fb3cd5bb0e9c3735fa6411
FIRST_NAME LAST_NAME <ibrahim.mowafy10@gmail.com>
2023-01-02 17:49:55 +0200

    Change styling in spinner component

--- components/spinner/Spinner.jsx
+++ components/spinner/Spinner.jsx
@@ -3,10 +3,10 @@ import classes from './spinner.module.css';
 
 const Spinner = () => {
   return (
-    <div class={classes.spinner}>
-      <div class={classes.bounce1}></div>
-      <div class={classes.bounce2}></div>
-      <div class={classes.bounce3}></div>
+    <div className={classes.spinner}>
+      <div className={classes.bounce1}></div>
+      <div className={classes.bounce2}></div>
+      <div className={classes.bounce3}></div>
     </div>
   );
 };
